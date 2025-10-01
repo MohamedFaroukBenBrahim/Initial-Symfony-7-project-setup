437eb88cae27a4bbbfeead57268f17a3f95b8548
MFBB 555 <benbrahimmohamed24@gmail.com>
2025-10-01 20:10:21 +0100

    Initial Symfony 7 project setup

---
+++ tests/Service/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Test\Service;
+
+use App\Service\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd(): void
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+}
+?>
